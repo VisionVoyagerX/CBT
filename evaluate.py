de5c661889d5dbc81c4bbf2325ab3d0429f155ed
nickdndndn <nickd_96@hotmail.com>
2023-08-11 13:54:22 +0300

    report

--- evaluate.py
+++ evaluate.py
@@ -171,8 +171,8 @@ def main(args):
 
     # load checkpoint
     if continue_from_checkpoint:
-        tr_metrics = load_checkpoint(torch.load(
-            checkpoint_path), model, optimizer, tr_metrics)
+        tr_metrics, val_metrics = load_checkpoint(torch.load(
+            checkpoint_path), model, optimizer, tr_metrics, val_metrics)
 
     """# evaluation mode
     model.eval()
