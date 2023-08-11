7289deb82f6a1f18bd256a957494d77e0bd8736d
nickdndndn <nickd_96@hotmail.com>
2023-08-11 14:04:52 +0300

    report

--- inference.py
+++ inference.py
@@ -195,7 +195,7 @@ def main(args):
 
             figure, axis = plt.subplots(nrows=1, ncols=4, figsize=(15, 5))
             axis[0].imshow((scaleMinMax(mslr.permute(0, 3, 2, 1).detach().cpu()[
-                            0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
+                            0, ...].numpy())).astype(np.float32)[..., :1], cmap='viridis')
             axis[0].set_title('(a) LR')
             axis[0].axis("off")
 
@@ -205,13 +205,13 @@ def main(args):
             axis[1].axis("off")
 
             axis[2].imshow((scaleMinMax(mssr.permute(0, 3, 2, 1).detach().cpu()[
-                            0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
+                            0, ...].numpy())).astype(np.float32)[..., :1], cmap='viridis')
             axis[2].set_title(
                 f'(c) CrossFormer {test_metric["psnr"]:.2f}dB/{test_metric["ssim"]:.4f}')
             axis[2].axis("off")
 
             axis[3].imshow((scaleMinMax(mshr.permute(0, 3, 2, 1).detach().cpu()[
-                            0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
+                            0, ...].numpy())).astype(np.float32)[..., :1], cmap='viridis')
             axis[3].set_title('(d) GT')
             axis[3].axis("off")
 
