22f9a25bbec560cdec6976e3e1690de7e0768e56
nickdndndn <nickd_96@hotmail.com>
2023-08-23 11:24:57 +0300

    report

--- model/deep_feature_extractor.py
+++ model/deep_feature_extractor.py
@@ -1133,7 +1133,7 @@ class MBAG(nn.Module):
                 norm_layer=norm_layer) for i in range(depth)
         ])
 
-        # SCBAB blocks
+        """# SCBAB blocks
         self.pan_scbab_blocks = nn.ModuleList([
             SCBAB(
                 dim=dim,
@@ -1161,7 +1161,7 @@ class MBAG(nn.Module):
                 drop=drop,
                 attn_drop=attn_drop,
                 norm_layer=norm_layer) for i in range(depth)
-        ])
+        ])"""
 
         # OCAB block
         self.pan_overlap_attn = OCAB(
@@ -1248,7 +1248,7 @@ class MBAG(nn.Module):
             mslr_forward = mslr_blk(
                 mslr_forward, x_size, params['rpi_sa'], params['attn_mask'])
 
-        # Multiple SCBAB
+        """# Multiple SCBAB
         for pan_blk, mslr_blk in zip(self.pan_scbab_blocks, self.mslr_scbab_blocks):
             pan_forward_temp = pan_blk(
                 pan_forward, mslr_forward, x_size, params['rpi_sa'], params['attn_mask'])
@@ -1256,7 +1256,7 @@ class MBAG(nn.Module):
                 mslr_forward, pan_forward, x_size, params['rpi_sa'], params['attn_mask'])
 
             pan_forward = pan_forward_temp
-            mslr_forward = mslr_forward_temp
+            mslr_forward = mslr_forward_temp"""
 
         # OCAB
         pan_forward = self.pan_overlap_attn(
